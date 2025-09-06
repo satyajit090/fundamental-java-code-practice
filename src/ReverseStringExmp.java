1ba0660e0a83945cea4056a22106df94d44ece4c
Satyajit Behera <satyajitbehera@Satyajits-MacBook-Air.local>
2025-09-06 13:38:05 +0530

    reverseString code

--- src/ReverseStringExmp.java
+++ src/ReverseStringExmp.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class ReverseStringExmp {
+
+//    public static void main(String[] args) {
+//
+//        String s= "Satyajit";
+//        String reverse = "";
+//
+//        for (int i = s.length()-1; i >=0 ; i--) {
+//
+//            reverse=reverse+s.charAt(i);
+//        }
+//        System.out.println(reverse);
+//    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.nextLine();
+        String rev= "";
+
+        for(int i=s.length()-1; i>=0; i--){
+            rev=rev+s.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+
 }
